88795f4fffba8f3137ab9ba7e6af484d40f86170
Miguel Ornia <UO250732@uniovi.es>
2019-12-06 13:31:49 +0100

    Proyecto RAA realizado con Unity y Vuforia

---
+++ Assets/MyScripts/DanceMoves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceMoves : MonoBehaviour {
+
+    public GameObject rocky;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = rocky.GetComponent<Animator> ();
+    }
+
+    public void Animacion1()
+    {
+        animator.SetTrigger("animacion1");
+    }
+
+    public void Animacion2()
+    {
+        animator.SetTrigger("animacion2");
+    }
+
+}
---
+++ Assets/MyScripts/VideoFondo.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class VideoFondo : MonoBehaviour, ITrackableEventHandler
+{
+    protected TrackableBehaviour mTrackableBehaviour;
+    public VideoPlayer videoFondo;
+
+    void Start () {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+       TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            //    Debug.Log("The Matrix>" + "Target detected");
+            videoFondo.Play();
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            //   Debug.Log("The Matrix>" + "Target lost");
+            videoFondo.Pause();
+            OnTrackingLost();
+        }
+        else
+        {
+            // Debug.Log("The Matrix>" + "Tracking starts");
+            videoFondo.Stop();
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+}
